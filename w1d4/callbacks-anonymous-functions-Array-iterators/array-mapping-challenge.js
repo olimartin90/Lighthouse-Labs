38edf67cd1681d6dcfa7bfcf37f3bba0ef5ff15d
Olivier Martin <olimartin90@gmail.com>
2018-06-07 18:34:02 +0000

    Array Mapping Challenge - Callback - Map Function

---
+++ w1d4/callbacks-anonymous-functions-Array-iterators/array-mapping-challenge.js
@@ -0,0 +1,17 @@
+var inputs = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = [];
+
+result = inputs.map(function(input) {
+  return Math.sqrt(Math.pow(input.x, 2) + Math.pow(input.y, 2));
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
+
+
