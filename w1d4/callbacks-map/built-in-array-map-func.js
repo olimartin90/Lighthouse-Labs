4847cdb593fd5eacde6fb13e7d09ceaeb5176300
Olivier Martin <olimartin90@gmail.com>
2018-06-07 21:19:10 +0000

    Built in array map function

--- w1d4/callbacks-map/built-in-array-map-func.js
+++ w1d4/callbacks-map/built-in-array-map-func.js
@@ -1,22 +1,22 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
 function map(arr, fct) {
-  var result = [];
-for (var i = 0; i < arr.length; i++) {
-  result.push(fct(arr[i]));
-}
-console.log(result);
-return result;
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        result.push(fct(arr[i]));
+    }
+    console.log(result);
+    return result;
 }
 
 map(words, function(word) {
-  return word.length;
+    return word.length;
 });
 
 map(words, function(word) {
-  return word.toUpperCase();
+    return word.toUpperCase();
 });
 
 map(words, function(word) {
-  return word.split('').reverse().join('');
+    return word.split('').reverse().join('');
 });
