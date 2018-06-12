ed2871309736de692b1f0f4fead1fb79efa13aad
Olivier Martin <olimartin90@gmail.com>
2018-06-12 01:37:28 +0000

    update code for implement downloadImageByURL

--- w2d1/github-avatar-downloader/download_avatars.js
+++ w2d1/github-avatar-downloader/download_avatars.js
@@ -1,7 +1,6 @@
 var request = require('request');
 var token = require('./secrets');
-
-// console.log('Welcome to the GitHub Avatar Downloader!');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -16,10 +15,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+
+// console.log('Downloading image...')
+
+request.get(url)
+
+.on('error', function(err) {
+    throw err;
+})
+
+.on('response', function(response) {
+    // console.log('Response Status Code: ', response.statusCode, ' Response Status Message: ', response.statusMessage, response.headers['content-type'])
+})
+
+.on('end', function() {
+    console.log('Download complete.')
+})
+
+.pipe(fs.createWriteStream(filePath));
+
+}
+
 getRepoContributors("jquery", "jquery", function(err, jsonString) {
   var jsObject = JSON.parse(jsonString);
   for (var user of jsObject) {
-    console.log(user.avatar_url);
+    downloadImageByURL(user.avatar_url, "./avatars/" + user.login + ".jpg")
   }
 });
-
